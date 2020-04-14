dd27b4a2873d4bc274391fa86cf114624807768b
jerome <jerome.saltmarsh@gmail.com>
2020-04-14 11:49:13 +0200

    stable

--- supersort.dart
+++ supersort.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 int totalArraysCreated = 0;
@@ -5,18 +6,35 @@ int comparisons = 0;
 int calls = 0;
 
 void main() {
-  const int total = 10000;
+  const int total = 10000000;
   print("Generating Random List");
-  List<int> unsortedList = generateRandomList(total: total, max: 100);
+  List<double> unsortedList = generateRandomDoubles(total: total, max: 1);
+//  print(unsortedList);
   try {
     print("Starting Sort");
-    List<List<int>> sortedPiles = pileSort(unsortedList);
+    List<List<double>> sortedPiles = pileSortDouble(unsortedList);
     print("Sort Finished");
 
-//    print("Unsorted");
-//    print(unsortedList);
-    print("Sorted Piles");
-    print(sortedPiles);
+    List<double> sortedNumbers =
+        sortedPiles.fold<List<double>>([], (all, pile) {
+      all.addAll(pile);
+      return all;
+    });
+
+//    if (sortedNumbers.length != unsortedList.length) {
+//      throw Exception(
+//          "Numbers missing. Found ${sortedNumbers.length}, expected ${unsortedList.length}");
+//    }
+//
+//    for (int i = 0; i < sortedNumbers.length - 1; i++) {
+//      if (sortedNumbers[i] > sortedNumbers[i + 1]) {
+//        throw Exception("Out of order index: $i, ${sortedNumbers[i]} ${sortedNumbers[i + 1]}");
+//      }
+//    }
+
+//    print("Sorted Piles");
+//    print(sortedPiles);
+//    print(sortedNumbers);
     print("Total Elements: $total");
     print("Total Arrays: $totalArraysCreated");
     print("Total Calls: $calls");
@@ -30,51 +48,43 @@ T call<T>(Function function) {
   return function();
 }
 
+List<double> pilesToList(List<List<double>> piles) {
+  return piles.fold<List<double>>([], (all, pile) {
+    all.addAll(pile);
+    return all;
+  });
+}
+
 List<List<int>> pileSort(List<int> unsortedList) {
   // if all the values are the same then return the list as it is
 //  if (unsortedList.every((value) => value == unsortedList[0])) {
 //    return [unsortedList];
 //  }
 
-  if (unsortedList.isEmpty) {
-    return [];
+  if (unsortedList.length < 2) {
+    return [unsortedList];
   }
 
   int min = unsortedList[0];
   int max = unsortedList[0];
 
-  int unsortedListLength = unsortedList.length;
-
-  for (int i = 0; i < unsortedListLength; i++) {
+  for (int i = 0; i < unsortedList.length; i++) {
     if (unsortedList[i] < min) {
       min = unsortedList[i];
-    } else if (unsortedList[i] > min) {
+    } else if (unsortedList[i] > max) {
       max = unsortedList[i];
     }
   }
-
-//  unsortedList.forEach((value) {
-//    if (call(() => value < min)) {
-//      min = value;
-//      return;
-//    }
-//
-//    if (call(() => value > max)) {
-//      max = value;
-//      return;
-//    }
-//  });
 
   if (max == min) {
     return [unsortedList];
   }
 
-//  int range = max - min;
-  double indexRatio = (max - min) / 100;
+  int range = max - min;
   Map<int, List<int>> piles = Map();
 
   unsortedList.forEach((value) {
-    double indexDouble = (value - min) * indexRatio;
+    double indexDouble = ((value - min) / range) * 100;
     int index = indexDouble.toInt();
     if (!piles.containsKey(index)) {
       totalArraysCreated++;
@@ -94,12 +104,83 @@ List<List<int>> pileSort(List<int> unsortedList) {
   return sortedPiles;
 }
 
+List<List<double>> pileSortDouble(List<double> unsortedList) {
+
+  calls++;
+  if (unsortedList.length < 2) {
+    return [unsortedList];
+  }
+
+  double min = unsortedList[0];
+  double max = unsortedList[0];
+
+  for (int i = 0; i < unsortedList.length; i++) {
+    if (unsortedList[i] < min) {
+      min = unsortedList[i];
+    } else if (unsortedList[i] > max) {
+      max = unsortedList[i];
+    }
+  }
+
+  if (max == min) {
+    return [unsortedList];
+  }
+
+  double range = max - min;
+  Map<int, List<double>> piles = Map();
+
+  unsortedList.forEach((value) {
+    double indexDouble = ((value - min) / range) * 100;
+    int index = indexDouble.toInt();
+    if (!piles.containsKey(index)) {
+      totalArraysCreated++;
+      piles[index] = [];
+    }
+    piles[index].add(value);
+  });
+
+  List<List<double>> sortedPiles = List();
+
+  for (int i = 0; i <= 100; i++) {
+    if (piles.containsKey(i)) {
+      List<double> pile = piles[i];
+      if (pile.length > 1) {
+        List<List<double>> subPiles = pileSortDouble(pile);
+        List<double> subList = pilesToList(subPiles);
+        sortedPiles.add(subList);
+      } else {
+        sortedPiles.add(pile);
+      }
+    }
+  }
+
+  return sortedPiles;
+}
+
 List<int> generateRandomList(
     {int total = 100, int max = 1000, bool includeNegatives = true}) {
-  Random random = Random();
+  Random random = Random(DateTime.now().microsecond);
   List<int> unsortedList = List();
   for (int i = 0; i < total; i++) {
     int value = random.nextInt(max);
+    if (includeNegatives) {
+      if (random.nextBool()) {
+        value = -value;
+      }
+    }
+
+    unsortedList.add(value);
+  }
+  return unsortedList;
+}
+
+List<double> generateRandomDoubles(
+    {int total = 100, int max = 1000, bool includeNegatives = true}) {
+
+  Random random = Random(DateTime.now().microsecond);
+  List<double> unsortedList = List();
+  for (int i = 0; i < total; i++) {
+    double value = random.nextDouble() * max;
     if (includeNegatives) {
       if (random.nextBool()) {
         value = -value;

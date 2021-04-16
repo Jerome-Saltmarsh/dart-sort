729946c4e3b3f3c1cb3233ac5312e0ad72501fe3
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-04-16 12:35:24 +0200

    implemented mega sort

--- fast-sort.dart
+++ fast-sort.dart
@@ -9,54 +9,102 @@ List<int> randomInts(int count, [int max = 1000]) {
   return numbers;
 }
 
-int i;
-int min;
-int max;
-double pivotValue;
-int itemsUnderPivot;
-int xValue;
-int yValue;
-int x;
-int y;
+int total = 0;
+int i = 0;
+int min = -1;
+int max = -1;
+int pivot = -1;
+int itemsUnderPivot = -1;
+int xValue = -1;
+int yValue = -1;
+int x = -1;
+int y = -1;
 
-void sort(List<int> items) {
+void mySort(List<int> items) {
   sortStartAndEnd(items, 0, items.length);
 }
 
-void sortStartAndEnd(List<int> items, int start, int end) {
-  min = items[start];
-  max = items[start];
-
-  // TODO only do this for first iteration, than pass limits down
-  for (i = start; i < end; i++) {
-    if (items[i] < min) {
-      min = items[i];
-    } else if (items[i] > max) {
-      max = items[i];
-    }
+bool isSorted(List<int> items, int start, int end) {
+  for (int index = start; index < end - 1; index++) {
+    if (items[index] > items[index + 1]) return false;
   }
+  return true;
+}
 
-  if (min == max) return;
+void sortStartAndEnd(List<int> items, int start, int end) {
+  DateTime flag0 = DateTime.now();
 
-  pivotValue = (min + max) * 0.5;
+  // calculate pivot
+  total = 0;
+  while (i < end) {
+    total += items[i];
+    i++;
+  }
+  pivot = total ~/ items.length;
+
+  DateTime flag1 = DateTime.now();
+
+  // calculate number of items under pivot
   itemsUnderPivot = 0;
-
   for (i = start; i < end; i++) {
-    if (items[i] < pivotValue) {
+    if (items[i] < pivot) {
       itemsUnderPivot++;
     }
   }
 
   if (itemsUnderPivot == 0) return;
 
-  // print(
-  //     "{start: $start, end: $end, min:$min, max:$max, 'pivot-value: $pivotValue, items-under-pivot-value: $itemsUnderPivot");
-  y = start + itemsUnderPivot;
-  for (x = start; x < start + itemsUnderPivot; x++) {
-    if (items[x] < pivotValue) {
+  DateTime flag2 = DateTime.now();
+
+  // swap items
+  int endX = start + itemsUnderPivot;
+  y = endX;
+  for (x = start; x < endX; x++) {
+    if (items[x] < pivot) {
       continue;
     }
-    while (items[y] >= pivotValue) {
+    xValue = items[x];
+    while (items[y] >= pivot) {
+      y++;
+    }
+    items[x] = items[y];
+    items[y] = xValue;
+  }
+
+  DateTime flag3 = DateTime.now();
+  printDifference('flag0', flag0, flag1);
+  printDifference('flag1', flag1, flag2);
+  printDifference('flag2', flag2, flag3);
+
+  // if (end - start < 2) return;
+
+  // if(!isSorted(items, start, itemsUnderPivot)){
+  //   sortStartAndEnd(items, start, itemsUnderPivot);
+  // }
+  // if(!isSorted(items, start + itemsUnderPivot, end)){
+  //   sortStartAndEnd(items, start + itemsUnderPivot, end);
+  // }
+}
+
+void sortStartAndEndPivot(
+    List<int> items, int start, int end, int min, int max) {
+  pivot = ((min + max) * 0.5).toInt();
+  itemsUnderPivot = 0;
+
+  for (i = start; i < end; i++) {
+    if (items[i] < pivot) {
+      itemsUnderPivot++;
+    }
+  }
+
+  if (itemsUnderPivot == 0) return;
+
+  y = start + itemsUnderPivot;
+  for (x = start; x < start + itemsUnderPivot; x++) {
+    if (items[x] < pivot) {
+      continue;
+    }
+    while (items[y] >= pivot) {
       y++;
     }
     xValue = items[x];
@@ -67,18 +115,147 @@ void sortStartAndEnd(List<int> items, int start, int end) {
     y++;
   }
 
-  if (end - start <= 2) return;
+  // if (end - start <= 2) return;
 
   // TODO remove call to self
-  sortStartAndEnd(items, start, itemsUnderPivot);
-  sortStartAndEnd(items, start + itemsUnderPivot, end);
+  // sortStartAndEndPivot(items, start, itemsUnderPivot, min, pivot.toInt());
+  // sortStartAndEndPivot(items, start + itemsUnderPivot, end, pivot.toInt(), max);
 }
 
-void main() {
-  List<int> list = randomInts(100);
-  print('*** random ***');
-  print(list);
-  sort(list);
-  print('*** sorted *** ');
-  print(list);
+int itemCount = 1000000;
+List<int> list = randomInts(itemCount);
+
+void randomizeList() {
+  list = randomInts(itemCount);
+}
+
+void testMySort() {
+  mySort(list);
+}
+
+void testOfficialSort() {
+  officialSort(list);
+}
+
+void officialSort(List<int> unsortedList) {
+  unsortedList.sort(quickSortInt);
+}
+
+int quickSortInt(int a, int b) {
+  if (a > b) {
+    return 1;
+  }
+  if (a < b) {
+    return -1;
+  }
+  return 0;
+}
+
+void timeFunction(Function function, {String name = 'some function'}) {
+  DateTime start = DateTime.now();
+  function();
+  DateTime finished = DateTime.now();
+  Duration duration = finished.difference(start);
+  int seconds = duration.inSeconds;
+  int milliseconds = duration.inMilliseconds % 1000;
+  print("Finished $name in $seconds seconds $milliseconds milliseconds");
+}
+
+void printDifference(String name, DateTime a, DateTime b) {
+  Duration duration = b.difference(a);
+  int seconds = duration.inSeconds;
+  int milliseconds = duration.inMilliseconds % 1000;
+  print("name: $name, seconds: $seconds, milliseconds:$milliseconds");
+}
+
+void insertionSort(List<int> a, int left, int right) {
+  for (int i = left + 1; i <= right; i++) {
+    var el = a[i];
+    int j = i;
+    while ((j > left) && (a[j - 1] > el)) {
+      a[j] = a[j - 1];
+      j--;
+    }
+    a[j] = el;
+  }
+}
+
+void megaSort(List<int> list) {
+  int length = list.length;
+  int lengthMinusOne = length - 1;
+  int min = 0;
+  int max = 0;
+
+  for (int i = 0; i < length; i++) {
+    if (list[i] < min) {
+      min = list[i];
+    } else if (list[i] > max) {
+      max = list[i];
+    }
+  }
+
+  int range = max - min;
+  int sectionWidth = range ~/  3;
+
+  int abPivot = min + sectionWidth;
+  int bcPivot = abPivot + sectionWidth;
+
+  int aSize = 0;
+  int bSize = 0;
+
+  for (int i = 0; i < length; i++) {
+    if (list[i] < abPivot) {
+      aSize++;
+    } else if (list[i] < bcPivot) {
+      bSize++;
+    }
+  }
+
+  // print('min: $min, max: $max, range: $range, abPivot: $abPivot, bcPivot: $bcPivot, aSize:$aSize, bSize: $bSize, cSize:${length - aSize - bSize}');
+
+  int indexA = 0;
+  int indexB = aSize;
+  int indexC = aSize + bSize;
+
+  int value = list[length - 1];
+  int swapValue = -1;
+
+  while (true) {
+
+    if (value < abPivot) {
+      while(list[indexA] < abPivot){
+        // print("${list[indexA]} already in section a.");
+        indexA++;
+      }
+      swapValue = value;
+      value = list[indexA];
+      list[indexA] = swapValue;
+      // print("$value swapped with ${swapValue} at $indexA");
+    }
+
+    else if (value < bcPivot) {
+      while(list[indexB] >= abPivot && list[indexB] < bcPivot){
+        indexB++;
+      }
+      swapValue = value;
+      value = list[indexB];
+      list[indexB] = swapValue;
+    }
+
+    else {
+      while(list[indexC] >= bcPivot){
+        indexC++;
+        if(indexC == lengthMinusOne){
+          return;
+        }
+      }
+      swapValue = value;
+      value = list[indexC];
+      list[indexC] = swapValue;
+
+      if(indexC == lengthMinusOne){
+        return;
+      }
+    }
+  }
 }
--- main.dart
+++ main.dart
@@ -1,103 +1,28 @@
-import 'block_sort.dart';
-import 'sort_double.dart';
-import 'utility.dart';
-
+import 'fast-sort.dart';
+import 'items.dart';
 
 void main() {
-  List<int> unsorted = generateRandomInts(total: 1000, max: 1000, includeNegatives: true);
+  randomItems = randomInts(100000);
+  timeFunction(sortRandomItems, name: 'sort Random items ${randomItems.length}');
+  // randomize();
+  // timeFunction(testOfficialSort, name: 'Officialt ${randomItems.length}');
+  // print(randomItems);
 
-  timeFunction((){
-//    List(100000000);
-//    List.filled(100000000, 0);
-//    skipAndSwap(unsorted);
-    skipAndSort(unsorted);
-//    unsorted.sort();
-  });
-
-//  print(unsorted);
 }
 
-dynamic testPileSortDouble(List<double> unsortedList) {
-  print("Starting PileSort of ${unsortedList.length} numbers");
-  DateTime start = DateTime.now();
-  var sorted = pileSortDouble(unsortedList);
-  DateTime finished = DateTime.now();
-  Duration duration = finished.difference(start);
-  int seconds = duration.inSeconds;
-  int milliseconds = duration.inMilliseconds % 1000;
-  print("PileSort Finished in $seconds seconds $milliseconds milliseconds");
-  return sorted;
+void randomize(){
+  randomItems.shuffle();
 }
 
-void timeFunction(Function function) {
-  DateTime start = DateTime.now();
-  function();
-  DateTime finished = DateTime.now();
-  Duration duration = finished.difference(start);
-  int seconds = duration.inSeconds;
-  int milliseconds = duration.inMilliseconds % 1000;
-  print("Finished in $seconds seconds $milliseconds milliseconds");
+void sortRandomItems(){
+  megaSort(randomItems);
 }
 
-
-//void testSortedIns(List<int>)
-
-List<double> testQuickSortDouble(List<double> unsortedList) {
-  print("Starting QuickSort of ${unsortedList.length} numbers");
-  DateTime start = DateTime.now();
-  unsortedList.sort(quicksortAlgorithmDouble);
-  DateTime finished = DateTime.now();
-  Duration duration = finished.difference(start);
-  int seconds = duration.inSeconds;
-  int milliseconds = duration.inMilliseconds % 1000;
-  print("QuickSort Finished in $seconds seconds $milliseconds milliseconds");
-  return unsortedList;
+void testOfficialSort(){
+  officialSort(randomItems);
 }
 
-void testQuickSortInt(List<int> unsortedList) {
-  print("Testing Quick Sort Int");
-  timeFunction(() {
-    unsortedList.sort(quickSortInt);
-  });
+void testMySort(){
+  megaSort(randomItems);
 }
 
-int quicksortAlgorithmDouble(double a, double b) {
-  if (a > b) {
-    return 1;
-  }
-  if (a < b) {
-    return -1;
-  }
-  return 0;
-}
-
-int quickSortInt(int a, int b) {
-  if (a > b) {
-    return 1;
-  }
-  if (a < b) {
-    return -1;
-  }
-  return 0;
-}
-
-void insertionSort(List<int> list) {
-  // If the same method could have both positional and named optional
-  // parameters, this should be (list, [start, end], {compare}).
-
-  for (int pos = 1; pos < list.length; pos++) {
-    int min = 0;
-    int max = pos;
-    var element = list[pos];
-    while (min < max) {
-      int mid = min + ((max - min) >> 1);
-      if (element < list[mid]) {
-        max = mid;
-      } else {
-        min = mid + 1;
-      }
-    }
-    list.setRange(min + 1, pos + 1, list, min);
-    list[min] = element;
-  }
-}

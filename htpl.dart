202e5ce236e9cf0cf0ee60379a37280f4e2012b0
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-04-17 18:37:31 +0200

    stable

--- htpl.dart
+++ htpl.dart
@@ -7,53 +7,57 @@
  * The point of the language is enable developers to
  */
 
-
-import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
+class Htpl {
+  List<Text> children;
+}
+
+class Text {
+  final String value;
+
+  Text(this.value);
+}
+
+
 String line1 = '<text: "hello world">';
 
-void main() async {
+void main(List<String> arguments) async {
   // read in the file .htpl
   // parse it and create a writer
   // save file to file system as hello_world.html
-  final path = 'C:/Users/Jerome/github/dart-sort/hello.htpl';
-  final File readFile = File(path);
-  final List<String> lines = [];
-  readFile
+  final helloFilePath = 'C:/Users/Jerome/github/dart-sort/hello.htpl';
+  final readFilePath = helloFilePath;
+  final File readFile = File(readFilePath);
+  final List<String> readFileLines = [];
+
+  print("importing $helloFilePath");
+  String writeContent = "";
+  await readFile
       .openRead()
       .map(utf8.decode)
       .transform(LineSplitter())
       .forEach((line) {
-    lines.add(line);
+    readFileLines.add(line);
     print('line added: $line');
+
+    if(line.startsWith('<text')){
+      writeContent = 'text directive detected';
+    }
   });
-  readFile.readAsString().then((String contents) {
-    print('contents: $contents');
-  });
+
+  print("imported ${readFile.path}");
 
 
-  final writeFile = File('exported.html');
-  final writtenFile = await writeFile.writeAsString('some content');
-  var sink = writeFile.openWrite();
-  sink.write('FILE ACCESSED ${DateTime.now()}\n');
-  // Close the IOSink to free system resources.
-  sink.close();
+  // readFile.readAsString().then((String contents) {
+  //   print('contents: $contents');
+  // });
 
-}
 
-void import() {
+  final writeFileName = readFile.path.replaceAll('htpl', 'html');
+  final writeFile = File(writeFileName);
+  final writtenFile = await writeFile.writeAsString(writeContent);
+  print('exported ${writeFile.absolute.path}');
 
-}
-
-void export() {
-
-}
-
-void generate() {
-  // read in a file
-  // import a html script
-  // write out to a new file
-  // export html script object to html, js and css files
 }

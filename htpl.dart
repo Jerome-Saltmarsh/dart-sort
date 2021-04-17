394aaae95a73a573196eab0fd026efaf44aca77a
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-04-17 19:43:17 +0200

    stable

--- htpl.dart
+++ htpl.dart
@@ -6,58 +6,44 @@
  *
  * The point of the language is enable developers to
  */
-
 import 'dart:io';
 import 'dart:convert';
 
-class Htpl {
-  List<Text> children;
-}
-
-class Text {
-  final String value;
-
-  Text(this.value);
-}
-
-
-String line1 = '<text: "hello world">';
 
 void main(List<String> arguments) async {
-  // read in the file .htpl
-  // parse it and create a writer
-  // save file to file system as hello_world.html
   final helloFilePath = 'C:/Users/Jerome/github/dart-sort/hello.htpl';
   final readFilePath = helloFilePath;
   final File readFile = File(readFilePath);
-  final List<String> readFileLines = [];
-
-  print("importing $helloFilePath");
+  final String nl = '\n';
   String writeContent = "";
+  final String empty = "";
+  writeContent += "<html>";
+  print("reading ${readFile.path}");
   await readFile
       .openRead()
       .map(utf8.decode)
       .transform(LineSplitter())
       .forEach((line) {
-    readFileLines.add(line);
-    print('line added: $line');
-
     if(line.startsWith('<text')){
-      writeContent = 'text directive detected';
+      final lineValue = line
+          .replaceAll("<text", empty)
+          .replaceAll(">", empty)
+          .replaceAll("\'", empty)
+          .replaceAll("value:", empty);
+
+      String attributes = "style=color:red";
+      String div = '<div $attributes>';
+      div += nl;
+      div += lineValue;
+      div += nl;
+      div += "<div/>";
+      writeContent += div;
     }
   });
-
-  print("imported ${readFile.path}");
-
-
-  // readFile.readAsString().then((String contents) {
-  //   print('contents: $contents');
-  // });
-
-
+  writeContent += "<html/>";
   final writeFileName = readFile.path.replaceAll('htpl', 'html');
   final writeFile = File(writeFileName);
-  final writtenFile = await writeFile.writeAsString(writeContent);
-  print('exported ${writeFile.absolute.path}');
-
+  await writeFile.writeAsString(writeContent);
+  print('writing ${writeFile.absolute.path}');
+  print(writeContent);
 }

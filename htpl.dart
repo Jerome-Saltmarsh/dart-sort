b6c906ee5cd43c9f960f71fb4c9f8b1c7ec83acd
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-04-18 08:39:38 +0200

    stable

--- htpl.dart
+++ htpl.dart
@@ -4,7 +4,9 @@
  * Hypertext progamming language is a computer programming language designed
  * by Jerome Saltmarsh (30) on the April 17 of 2021.
  *
- * The point of the language is enable developers to
+ * The point of the language is enable developers to write elegent code for the web
+ *
+ * html is not elegent
  */
 import 'dart:io';
 import 'dart:convert';
@@ -27,20 +29,34 @@ void main(List<String> arguments) async {
     if(line.startsWith('<text')){
       final lineValue = line
           .replaceAll("<text", empty)
-          .replaceAll(">", empty)
-          .replaceAll("\'", empty)
-          .replaceAll("value:", empty);
+          .replaceAll(">", empty);
+          // .replaceAll("\'", empty);
 
-      String attributes = "style=color:red";
-      String div = '<div $attributes>';
+      List<String> parts = lineValue.split(" ");
+      // Map<String, dynamic> attributes = Map();
+      // parts.forEach((part){
+      //   List<String> subParts = part.split(":");
+      //   attributes[subParts[0]] = subParts[1];
+      // });
+
+      // "style=color:blue;
+      String attributeString = "onclick=\"printHelloWorld()\"";
+      String div = '<div $attributeString>';
       div += nl;
       div += lineValue;
       div += nl;
-      div += "<div/>";
+      div += "</div>";
       writeContent += div;
     }
   });
-  writeContent += "<html/>";
+  writeContent += "</html>";
+  writeContent += "<script>";
+  writeContent += 'function printHelloWorld() {';
+  // writeContent += 'document.getElementById("demo").innerHTML = "YOU CLICKED ME!";';
+  writeContent += 'console.log("hello");';
+  writeContent += 'alert("hello");';
+  writeContent += '}';
+  writeContent += "</script>";
   final writeFileName = readFile.path.replaceAll('htpl', 'html');
   final writeFile = File(writeFileName);
   await writeFile.writeAsString(writeContent);

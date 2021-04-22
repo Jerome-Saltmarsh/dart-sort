92914be7ded3605d2ff2a56f35643af0b8994233
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-04-22 17:48:36 +0200

    added split sort

--- main.dart
+++ main.dart
@@ -2,36 +2,88 @@ import 'dart:math';
 
 import 'dart-sort.dart';
 import 'items.dart';
+import 'split-sort.dart';
+
+int swaps = 0;
+int comparisons = 0;
 
 void main() {
+  testSplitSort();
   // items = items;
-  // items = generate_random_list(1000);
   // // time_function(master_sort_items, name: 'master-sort ${items.length} items');
-  // // time_function(quick_sort_items, name: 'Official ${items.length}');
+  // time_function(quick_sort_items, name: 'Official ${items.length}');
   // time_function(good_sort_items, name: 'Good Sort ${items.length}');
+  // time_function(testSortMany, name: 'Good Sort ${items.length}');
   // // insertionSort(items, 0, items.length - 1);
   // print(items);
 
   // List<int> list8 = [0, 12, 5, 8, 8, 3, 9, 10];
-  List<int> list16 = [5, 12, 5, 8, 8, 25, 9, 10, 32, 4, 22, 88, 16, 45, 31, 5];
   // sort8Indexes(list8, 0, 1, 2, 3, 4, 5, 6, 7);
-  sort16Indexes(list16, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+  // List<int> list16 = [
+  //   5,
+  //   112,
+  //   5,
+  //   18,
+  //   8,
+  //   25,
+  //   9,
+  //   10,
+  //   32,
+  //   4,
+  //   22,
+  //   88,
+  //   16,
+  //   45,
+  //   31,
+  //   5
+  // ];
+  // sort16Indexes(list16, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+
+  // print(list8);
+
+  // print('swaps: $swaps, comparisons: $comparisons');
+}
+
+void testSortMany(){
+  items = items;
+  int margin = items.length ~/ 16;
+
+  for (int x = 0; x < margin; x++) {
+    sort16Indexes(
+        items,
+        x,
+        x + margin,
+        x + margin * 2,
+        x + margin * 3,
+        x + margin * 4,
+        x + margin * 5,
+        x + margin * 6,
+        x + margin * 7,
+        x + margin * 8,
+        x + margin * 9,
+        x + margin * 10,
+        x + margin * 11,
+        x + margin * 12,
+        x + margin * 13,
+        x + margin * 14,
+        x + margin * 15);
+  }
 }
 
 /**
  * An extremely efficient sorting algorithm
  */
-
-int swaps = 0;
-
 bool sort2Indexes(List<int> list, int i1, int i2) {
   int v1 = list[i1];
   int v2 = list[i2];
+  comparisons++;
   if (list[i1] > list[i2]) {
     list[i2] = v1;
     list[i1] = v2;
     swaps++;
-    print(list);
+    // print(list);
     return true;
   }
   return false;
@@ -75,15 +127,13 @@ void sort16Indexes(
     int i13,
     int i14,
     int i15,
-    int i16
-    ) {
-
+    int i16) {
   sort8Indexes(list, i1, i2, i3, i4, i5, i6, i7, i8);
   sort8Indexes(list, i9, i10, i11, i12, i13, i14, i15, i16);
 
-  if(sort2Indexes(list, i8, i9)){
-    if(sort2Indexes(list, i7, i10)){
-      if(sort2Indexes(list, i6, i11)){
+  if (sort2Indexes(list, i8, i9)) {
+    if (sort2Indexes(list, i7, i10)) {
+      if (sort2Indexes(list, i6, i11)) {
         sort2Indexes(list, i5, i12);
       }
     }
@@ -92,13 +142,33 @@ void sort16Indexes(
   sort8Indexes(list, i1, i2, i3, i4, i5, i6, i7, i8);
   sort8Indexes(list, i9, i10, i11, i12, i13, i14, i15, i16);
 
-  if(sort2Indexes(list, i8, i9)){
-    if(sort2Indexes(list, i7, i10)){
-      if(sort2Indexes(list, i6, i11)){
+  if (sort2Indexes(list, i8, i9)) {
+    if (sort2Indexes(list, i7, i10)) {
+      if (sort2Indexes(list, i6, i11)) {
         sort2Indexes(list, i5, i12);
       }
     }
   }
+
+  sort2Indexes(list, 0, 1);
+
+  // print([
+  //     list[i1],
+  //     list[i2],
+  //     list[i3],
+  //     list[i4],
+  //     list[i5],
+  //     list[i6],
+  //     list[i7],
+  //     list[i8],
+  //     list[i9],
+  //     list[i10],
+  //     list[i11],
+  //     list[i12],
+  //     list[i13],
+  //     list[i14],
+  //     list[i15],
+  //     list[i16]]);
 }
 
 void shuffle_items() {
@@ -142,3 +212,5 @@ void printDifference(String name, DateTime a, DateTime b) {
   int milliseconds = duration.inMilliseconds % 1000;
   print("name: $name, seconds: $seconds, milliseconds:$milliseconds");
 }
+
+
---
+++ split-sort.dart
@@ -0,0 +1,44 @@
+void testSplitSort() {
+  // splitSort([7, 4, 12, 10, 11, 1, 12, 2]);
+  splitSort([7, 4, 12, 10, 11, 1, 12, 2, 5, 20, 3, 9, 14, 99, 35]);
+}
+
+void splitSort(List<int> list) {
+  print(list);
+  int pivot = list.length ~/ 2;
+  _splitSort(list, 0, pivot);
+  _splitSort(list, pivot, list.length - 1);
+  mergePivots(list, pivot);
+}
+
+void _splitSort(List<int> list, int left, int right) {
+  if (left + 1 == right) {
+    if (list[left] > list[left + 1]) {
+      int val = list[left];
+      list[left] = list[left + 1];
+      list[left + 1] = val;
+      print(list);
+    }
+    return;
+  }
+
+  int pivot = (left + right) ~/ 2;
+  _splitSort(list, left, pivot);
+  _splitSort(list, pivot, right);
+  mergePivots(list, pivot);
+}
+
+void mergePivots(List<int> list, int pivot) {
+  int swaps = 0;
+  while (pivot + swaps < list.length && pivot -1 - swaps > 0 && list[pivot + swaps] < list[pivot - 1 - swaps]) {
+    swaps++;
+  }
+
+  for (int i = 0; i < swaps; i++) {
+    int val1 = list[pivot - i - 1];
+    int val2 = list[pivot + swaps - i - 1];
+    list[pivot - i - 1] = val2;
+    list[pivot + swaps - i - 1] = val1;
+    print(list);
+  }
+}
